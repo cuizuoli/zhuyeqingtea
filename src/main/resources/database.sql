13e3940c57fb526900717b7b7efd868110729dcc
piaozaiguang <zaiguang.piao@gmail.com>
2013-12-31 15:11:36 +0800

    添加参与表，商品表，奖赏表

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -13,16 +13,30 @@ CREATE TABLE `weibo_user` (
   `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
--- 分享表
-DROP TABLE IF EXISTS `share`;
-CREATE TABLE `share` (
-  `weibo_id` varchar(20) NOT NULL COMMENT '微博ID',
+-- 参与表
+DROP TABLE IF EXISTS `join_hist`;
+CREATE TABLE `join_hist` (
+  `join_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '参与ID',
   `user_id` varchar(20) NOT NULL COMMENT '用户ID',
-  `content` text NOT NULL COMMENT '微博内容',
-  `share_type` char(1) NOT NULL COMMENT '分享类别',
-  `repost_count` int(11) DEFAULT NULL COMMENT '转发数量',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`weibo_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='分享表';
-
+  `join_time` datetime DEFAULT NULL COMMENT '参与时间',
+  PRIMARY KEY (`join_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='参与表';
+-- 商品表
+DROP TABLE IF EXISTS `item_info`;
+CREATE TABLE `item_info` (
+  `item_id` int(3) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+  `item_level` int(1) NOT NULL COMMENT '0:特等, 1:1等奖, 2:2等奖, 3:3等奖, 4:爱心贡献奖',
+  `item_name` varchar(20) DEFAULT NULL COMMENT '商品名称',
+  `reward_rate` int(1) DEFAULT 0 COMMENT '中奖几率',
+  `active_yn` boolean DEFAULT false COMMENT '是否启用',
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品表';
+-- 奖赏表
+DROP TABLE IF EXISTS `reward_hist`;
+CREATE TABLE `reward_hist` (
+  `reward_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '奖赏ID',
+  `user_id` varchar(20) NOT NULL COMMENT '用户ID',
+  `item_id` int(3) DEFAULT NULL COMMENT '商品ID',
+  `reward_time` datetime DEFAULT NULL COMMENT '奖赏时间',
+  PRIMARY KEY (`reward_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='奖赏表';
