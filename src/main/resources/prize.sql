aafb47bf49f046f8d67a8b76e6404258f90bde2e
cuizuoli <cuizuoli@cuizuoli-PC>
2014-01-10 23:43:34 +0800

    added show prize sql.

--- src/main/resources/prize.sql
+++ src/main/resources/prize.sql
@@ -6,3 +6,15 @@ WHERE prize_id = 1;
 UPDATE prize
 SET active_yn = 'N'
 WHERE prize_id IN(2,3,4,5);
+
+SELECT
+u.user_id,
+u.user_name,
+p.prize_name,
+COUNT(1)
+FROM reward r
+INNER JOIN weibo_user u
+ON u.user_id = r.user_id
+INNER JOIN prize p
+ON p.prize_id = r.prize_id
+GROUP BY r.user_id,r.prize_id
